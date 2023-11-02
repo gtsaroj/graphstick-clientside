7ca5aed8dfbd9cb024c0998c3d0e617202736dcb
gtsaroj <sarojgt326@gmail.com>
2023-11-02 15:32:35 +0545

    final update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store, persistor } from "./CardReducer/store"
-import { PersistGate } from 'redux-persist/integration/react'
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import App from './App';
+  import { Provider } from 'react-redux';
+  import { store, persistor } from "./CardReducer/store"
+  import { PersistGate } from 'redux-persist/integration/react'
 
 
-const root = document.getElementById('root');
-ReactDOM.createRoot(root).render
-  (
-    <React.StrictMode>
-      <Provider store={store} >
-        <PersistGate loading={"Loading"} persistor={persistor}>
-          <App />
-        </PersistGate>
+  const root = document.getElementById('root');
+  ReactDOM.createRoot(root).render
+    (
+      <React.StrictMode>
+        <Provider store={store} >
+          <PersistGate loading={"Loading"} persistor={persistor}>
+            <App />
+          </PersistGate>
 
-      </Provider>
-    </React.StrictMode>
+        </Provider>
+      </React.StrictMode>
 
-  );
+    );
